a604d8fbff1b00458907fa4c8358e0ce2907baec
Macno85 <m_castagno@hotmai.com>
2015-05-05 10:37:55 -0300

    Update 1.pl

--- chapter2/1.pl
+++ chapter2/1.pl
@@ -1,4 +1,19 @@
-#!/usr/bin/perl -w
+=beg
+print "Enter a first name: "; # The prompt (no new-line)
+$name = <STDIN>; # Read from standard input
+chomp $name; # Remove new-line character
+
+Exercise 1
+Using the code fragment above as a guide, prompt the user for a first name then a
+last name, storing these in different scalar variables.
+Now transfer the contents of both these scalars into an array, using a list assignment.
+Print the array with interpolation, not forgetting the new-line character.
+Now put the first and last names into a hash, using the keys 'first' and 'last'.
+Is it possible to print the hash?
+Dump the hash into the array, using a single assignment, and print the array again.
+Can you explain what is printed?
+
+=cut
 
 print "Enter a first name: ";
 $first = <STDIN>;
