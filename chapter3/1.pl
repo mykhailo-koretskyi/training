b441ea74d3e9d23382fe099290edbacfdd5ea78d
Macno85 <m_castagno@hotmai.com>
2015-05-05 10:44:59 -0300

    Update 1.pl

--- chapter3/1.pl
+++ chapter3/1.pl
@@ -1,3 +1,15 @@
-#!/usr/bin/perl -w
+=begin
+The command line arguments for a script are available within the program from a predefined
+system variable @ARGV.
+Write a program, echo.pl, that prints all its command line arguments to the screen.
+Call the program with a shell wildcard (glob construct), for example:
+On UNIX:
+echo.pl /etc/*
+On Microsoft Windows NT/2000:
+perl –w echo.pl c:\winnt\*
+On Microsoft Windows 95/98/Me/XP:
+perl –w echo.pl c:\windows\*
+What happens? Which program is responsible for the handling of wildcards?
+=cut
 
 print "@ARGV";
