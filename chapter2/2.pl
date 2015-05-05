7421633adfb2231d0ba592d861be4f22a168fc47
Macno85 <m_castagno@hotmai.com>
2015-05-05 10:40:51 -0300

    Update 2.pl

--- chapter2/2.pl
+++ chapter2/2.pl
@@ -1,4 +1,11 @@
-#!/usr/bin/perl -w
+=begin
+
+Create an array @numbers with the five numbers: 1, 5, 9, 3.14159 and 2. Print it with
+and without interpolation ("@numbers" and @numbers), and note differences.
+Using the predefined variable $", print each element of the array on a different line
+using a single print statement.
+
+=cut
 
 @numbers = (1, 5, 9, 3.14159, 2);
 
