a4b7ebfbd0a5f06d0beba554fcca701c0419a026
Macno85 <m_castagno@hotmai.com>
2015-05-05 10:42:30 -0300

    Update 3.pl

--- chapter2/3.pl
+++ chapter2/3.pl
@@ -1,4 +1,13 @@
-#!/usr/bin/perl -w
+=begin
+Write a Perl program that reads a string $text (see above), then print it using each
+of the interpolations, \l \L \u \U \Q (see Page 9 of this chapter). For example,
+using \U means: print "\U$text\n";.
+Try this with various input values:
+(a) A number, e.g. 123
+(b) A name, e.g. John
+(c) A sentence, e.g. How are you today?
+(d) Non-alphanumeric character sequences, e.g. * & % $ \
+=cut
 
 print "Enter a text: ";
 $text = <STDIN>;
