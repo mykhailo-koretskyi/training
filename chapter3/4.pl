5d161fba074bf12e261537be5052c38c557825c6
Macno85 <m_castagno@hotmai.com>
2015-05-05 10:48:13 -0300

    Update 4.pl

--- chapter3/4.pl
+++ chapter3/4.pl
@@ -1,3 +1,11 @@
+=begin
+Create an array from a list of eight computer names:
+yogi, booboo, grizzly, rupert, baloo, teddy, bungle, care
+now write code to alter the array:
+i. Change 'baloo' to 'greppy'
+ii. Add 'fozzie' to the end of the array
+iii. Print the resulting array, and its length
+=cut
 
 @names = qw(yogi booboo grizzly rupert baloo teddy bungle care);
 
