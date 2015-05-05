7b739f8c482d7b32b0d3c1b78099f0862a81303a
Macno85 <m_castagno@hotmai.com>
2015-05-05 10:48:57 -0300

    Update 5.pl

--- chapter3/5.pl
+++ chapter3/5.pl
@@ -1,3 +1,16 @@
+=begin
+This question is concerned with array slices.
+Using the array created in the previous exercise:
+· In one statement, copy the last four elements of the array into a list of
+scalar variables, one for each element (you can call the scalars
+anything you like). Print the four scalars.
+· In one statement, copy the first element of the array into a scalar and
+the next three elements into a new array called @new. Print the
+scalar and the new array.
+· In one statement, add @new onto the end of the array (this does not
+involve a slice), then print the result.
+=cut
+
 @names = qw(yogi booboo grizzly rupert baloo teddy bungle care);
 $names[4] = 'greppy';
 $names[@names] = 'fozzie';
