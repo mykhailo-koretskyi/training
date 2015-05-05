2dbc315d595e43a8d6c66c87eab898ca7e86bd7a
Macno85 <m_castagno@hotmai.com>
2015-05-05 10:46:48 -0300

    Update 2.pl

--- chapter3/2.pl
+++ chapter3/2.pl
@@ -1,3 +1,15 @@
+=begin
+Create a hash that contains the following first-name => last-name pairs:
+John Norman
+Robert Anson
+Christopher Fowler
+Robert Harris
+Dan Simmons
+Now add code to prompt for a first name. Use the reply to obtain the last name from
+the hash, and print the full name.
+What happened to the common first names (Robert)? Hint: try swapping them
+around.
+=cut
 
 %hash = (
 	John => "Norman",
